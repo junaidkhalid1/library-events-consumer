88c309a3cf8aac36962ef683b19e9553097e6601
Junaid KHALID <junaidkhalid00007@gmail.com>
2021-12-31 20:33:33 +0100

    Manual Consumer Offset Management

---
+++ src/main/java/com/learnkafka/library/consumer/LibraryEventsConsumerManualOffset.java
@@ -0,0 +1,20 @@
+package com.learnkafka.library.consumer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.listener.AcknowledgingMessageListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class LibraryEventsConsumerManualOffset implements AcknowledgingMessageListener<Integer,String> {
+
+    @Override
+    @KafkaListener(topics = {"library-events"}) // by default applying containerFactory = "kafkaListenerContainerFactory" from KafkaAnnotationDrivenConfiguration.java
+    public void onMessage(ConsumerRecord<Integer, String> consumerRecord, Acknowledgment acknowledgment) {
+        log.info("ConsumerRecord : {} ", consumerRecord);
+        acknowledgment.acknowledge();// after record is processed, it's up to developer to manually commit that acknowledge method
+    }
+}
